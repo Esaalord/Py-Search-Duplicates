4f1f5404c188d0b1dfeb6b86fae37f45b02c8b07
NyanKiyoshi <nekowdevzhdd2.0.hackdowemocore@gmail.com>
2014-06-17 02:22:54 +0200

    License header added into Py files and md5sum was moved for more readability. + Typo

--- search.py
+++ search.py
@@ -1,3 +1,30 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+#
+#  Py-Search-Duplicates (https://github.com/NyanKiyoshi/Py-Search-Duplicates/)
+# A Python2 script to search all duplicates files into a given directory recursively.
+#
+# Copyright (c) 2014 - NyanKiyoshi @ https://github.com/NyanKiyoshi/
+#
+#                           The MIT License (MIT)
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from md5sum import md5sum as mds
 from os import walk, makedirs, listdir, getcwd
 from shutil import rmtree
